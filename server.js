68080d57b3f71181c7d42d9557d33c9a4810ba48
Mike <62777339+memarino92@users.noreply.github.com>
2020-07-27 16:59:17 -0400

    added functionality to server api

--- server.js
+++ server.js
@@ -8,3 +8,40 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT);
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.send({
+       quote: randomQuote
+    });
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const person = req.query.person;
+    if (person) {
+        const arrayToSend = quotes.filter(quote => quote.person === person);
+        res.send({
+            quotes: arrayToSend
+        });
+    } else {
+        res.send({
+            quotes: quotes
+        });
+    }
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    if (!req.query.quote || !req.query.person) {
+        res.status(400).send('Invalid format');
+    } else {
+        const newQuote = {
+            quote: req.query.quote,
+            person: req.query.person
+        };
+        quotes.push(newQuote);
+        res.status(201).send({
+            quote: newQuote
+        });
+    }
+});
